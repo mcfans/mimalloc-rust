d116224ecc130ea0b62b272e167e05a80f0573d4
mcfans <mcfans@users.noreply.github.com>
2026-04-06 20:30:03 +0800

    reduce segment size from 4MiB to 2MiB on 64-bit
    Further reduce per-thread memory footprint for memory-constrained
    iOS environments. MI_LARGE_OBJ_SIZE_MAX becomes 1MiB.

--- mimalloc-rust-sys/mimalloc/include/mimalloc/types.h
+++ mimalloc-rust-sys/mimalloc/include/mimalloc/types.h
@@ -160,7 +160,7 @@ typedef int32_t  mi_ssize_t;
 #define MI_SEGMENT_SLICE_SHIFT            (13 + MI_INTPTR_SHIFT)         // 64KiB  (32KiB on 32-bit)
 
 #if MI_INTPTR_SIZE > 4
-#define MI_SEGMENT_SHIFT                  ( 6 + MI_SEGMENT_SLICE_SHIFT)  // 4MiB
+#define MI_SEGMENT_SHIFT                  ( 5 + MI_SEGMENT_SLICE_SHIFT)  // 2MiB
 #else
 #define MI_SEGMENT_SHIFT                  ( 7 + MI_SEGMENT_SLICE_SHIFT)  // 4MiB on 32-bit
 #endif
